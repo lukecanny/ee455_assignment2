b092f5d9f44befc3ee5728013c257ecea9fd8aa3
lukecanny <lukecanny@protonmail.ch>
2022-11-22 20:15:04 +0000

    Added for loop to test multiple values of filter length (by adjusting order, n)

--- part2.m
+++ part2.m
@@ -3,7 +3,7 @@
 %   Description: This script generates the required plots for Part B of
 %   Assignment 2 EE445 (Digital Signal Processing)
 %
-
+close all
 % Declaring inital variables
 fsamp           = 10000;    % Sampling Frequency
 fcut            = 3000;     % Cut-off Frequency
@@ -14,6 +14,12 @@ gainStop        = 0;        % Stopband Gain
 f = [0 fcut/(fsamp/2) (fcut+transitionBW)/(fsamp/2) 1];
 a = [1 1 0 0];
 
-% Applying firpm function
-b = firpm(20, f, a);
-freqz(b,1,512);
+% For loop runs 6 times - i used to derive n = 20, 25, 30...50
+for i = 1:7
+    figure;
+    n = 15 + (i * 5);
+    % Applying firpm function
+    b = firpm(n, f, a);
+    freqz(b,1,512);
+    title("Plot of X against Y where n = " + n);
+end
