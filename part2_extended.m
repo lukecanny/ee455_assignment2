4e90c190f181e6acd18d56c3750bab287340ccbf
lukecanny <lukecanny@protonmail.ch>
2022-11-22 20:31:11 +0000

    Uses fir1 function to plot magnitude response - second half of part 2

---
+++ part2_extended.m
@@ -0,0 +1,21 @@
+%   Author: Luke Canny 19339166
+%   Date:   16/11/22
+%   Description: This script generates the required plots for Part B of
+%   Assignment 2 EE445 (Digital Signal Processing)
+%
+close all
+% Declaring inital variables
+fsamp           = 10000;    % Sampling Frequency
+fcut            = 3000;     % Cut-off Frequency
+transitionBW    = 400;      % Transition Bandwidth
+gainStop        = 0;        % Stopband Gain
+
+% Deriving variables required to use firpm function
+f = [0 fcut/(fsamp/2) (fcut+transitionBW)/(fsamp/2) 1];
+a = [1 1 0 0];
+
+% Applying fir1 function
+n = 50;
+b = fir1(n, fcut/(fsamp/2));
+freqz(b,1,512);
+title("Plot of Magnitude Response - n = " + n);
