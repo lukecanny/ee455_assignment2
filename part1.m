6d780dd92fd680d943adb3ba36ec9ad976d03021
lukecanny <lukecanny@protonmail.ch>
2022-11-22 18:48:04 +0000

    Applying cheb1ord function

--- part1.m
+++ part1.m
@@ -10,3 +10,12 @@ fcut            = 3500;     % Cut-off Frequency
 transitionBW    = 400;      % Transition Bandwidth
 Astop           = 30;       % Stop band attenuation
 dPass           = 0.1;      % Passband Ripple
+
+% Applying cheb1ord function
+Wp = fcut/(fsamp/2);
+Ws = (fcut+transitionBW)/(fsamp/2);
+Rp = dPass;
+Rs = Astop;
+[n,Wp] = cheb1ord(Wp,Ws,Rp,Rs);
+
+% Applying cheby1 function
